7bbf65def7e047fdbf3d743480cdc130f37d4765
rita-j-santos-alb <rita.j.santos@present-technologies.com>
2024-09-11 11:22:12 +0100

    new method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
   public static void main(String[] args) {
       System.out.println("Hello, World!");
-      System.out.println("I am doing th first commit on this branch");
+      try {
+          printMessage("Welcome to Java Programming!");
+      } catch (Exception e) {
+          System.err.println("An error occurred: " + e.getMessage());
+      }
   }
 
-  public static int addNumbers(int a, int b) {
-      return a + b;
+  public static void printMessage(String message) throws Exception {
+      if (message == null) {
+          throw new Exception("Message cannot be null!");
+      }
+      System.out.println(message);
   }
 }
