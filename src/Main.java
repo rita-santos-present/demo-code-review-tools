1ebad941435dfa1f2a8ec56eff3a5106868ab801
rita-j-santos-alb <rita.j.santos@present-technologies.com>
2024-09-11 12:17:38 +0100

    logical errors

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-public class main {
-public static void main(String[] args) {
-System.out.println("Hello, World!");
-}
-
-// Method name does not follow camelCase
-public static void print_message(String message) {
-System.out.println(message);
-}
+public class Main {
+  public static void main(String[] args) {
+      int number = 10;
+      // Incorrect logic: should check if number is greater than 10
+      if (number < 10) {
+          System.out.println("Number is greater than 10");
+      } else {
+          System.out.println("Number is 10 or less");
+      }
+  }
 }
