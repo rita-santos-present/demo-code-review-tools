546923d8649673bf323578fed007bdcd4f5e2789
rita-j-santos-alb <rita.j.santos@present-technologies.com>
2024-09-11 12:22:33 +0100

    null pointer exception

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-      int number = 10;
-      // Incorrect logic: should check if number is greater than 10
-      if (number < 10) {
-          System.out.println("Number is greater than 10");
-      } else {
-          System.out.println("Number is 10 or less");
-      }
+      String message = null;
+      // Potential NullPointerException
+      System.out.println(message.toUpperCase());
   }
 }
