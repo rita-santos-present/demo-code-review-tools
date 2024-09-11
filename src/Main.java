07bfc59a902b493644ad981bd5ba1676541558d9
rita-j-santos-alb <rita.j.santos@present-technologies.com>
2024-09-11 11:00:11 +0100

    1st commit tst-1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
       System.out.println("Hello, World!");
+      System.out.println("I am doing th first commit on this branch");
   }
 
   public static int addNumbers(int a, int b) {
